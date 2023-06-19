7cd7c3486e35bd0d227e95d257969224408bb501
James Charlesworth <jcharlesworthuk@gmail.com>
2023-06-19 10:39:50 +0100

    typescript starter working with modules and env file

---
+++ README.md
@@ -0,0 +1,7 @@
+
+Add a `.env` file with the following:
+
+```
+OPENAI_KEY=<your key>
+```
+
--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import { Configuration, OpenAIApi } from "openai";
+
+type ProcessEnv = {
+    OPENAI_KEY: string
+}
+
+const env: ProcessEnv = <ProcessEnv>process.env;
+
+console.log(env.OPENAI_KEY);
+
+const openai = new OpenAIApi(new Configuration({
+    apiKey: env.OPENAI_KEY
+}));
 console.log('Hello world');
